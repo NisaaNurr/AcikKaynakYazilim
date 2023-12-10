328c914a18aa0dca8703483c8593ae483f25e611
NisaaNurr <Celiknisanur5768@gmail.com>
2023-12-10 00:46:50 +0000

    python kodu degistirilidi 5

--- ornek.py
+++ ornek.py
@@ -9,39 +9,37 @@ class Users(Resource):
     def get(self):
         data = pd.read_csv('havadurumu.csv')
         data = data.to_dict('records')
-        return {'data' : data}, 200
+        return {'data': data}, 200
 
     def post(self):
-        il=request.args['İl']
-        sıcaklık=request.args['sıcaklık']
-        havadurumu=request.args['havadurumu']
+        il = request.args['İl']
+        sıcaklık = request.args['sıcaklık']
+        havadurumu = request.args['havadurumu']
 
         req_data = pd.DataFrame({
-            'İl'    : ['İl'],
-            'sıcaklık'    : ['sıcaklık'],
-            'havadurumu'      : ['havadurmu']
-
+            'İl': [il],
+            'sıcaklık': [sıcaklık],
+            'havadurumu': [havadurumu]
         })
-        data = pd.read_csv('users.csv')
-        #data = pd.concat([data, req_data], ignore_index=True)
-        data=data.append(req_data,ignore_index=True)
-        data.to_csv('users.csv', index=False)
-        return {'message' : 'Record successfully added.'}, 200
+
+        data = pd.read_csv('havadurumu.csv')
+        data = data.append(req_data, ignore_index=True)
+        data.to_csv('havadurumu.csv', index=False)
+        return {'message': 'Record successfully added.'}, 200
 
 
 class Name(Resource):
-    def get(self,name):
+    def get(self, name):
         data = pd.read_csv('havadurumu.csv')
         data = data.to_dict('records')
         for entry in data:
-            if entry['name'] == name :
-                return {'data' : entry}, 200
-        return {'message' : 'İlin hava durumu bilgisi bulunamadı ! '}, 404
-# Add URL endpoints
-api.add_resource(havadurumu, '/users')
-api.add_resource(İl, '/<string:İl>')
+            if entry['İl'] == name:
+                return {'data': entry}, 200
+        return {'message': 'İlin hava durumu bilgisi bulunamadı!'}, 404
 
+# Add URL endpoints
+api.add_resource(Users, '/users')
+api.add_resource(Name, '/<string:name>')
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0", port=6767)
     app.run()
