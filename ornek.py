9ac222639ac9088b98b09028f2423f1b4416eca9
NisaaNurr <Celiknisanur5768@gmail.com>
2023-12-10 15:54:09 +0000

    havadurumu.csv code revize 3

--- ornek.py
+++ ornek.py
@@ -16,30 +16,31 @@ class Users(Resource):
         Sicaklik = request.args['Sicaklik']
         havadurumu = request.args['havadurumu']
 
-        req_data = pd.DataFrame({
+	data = pd.read_csv('havadurumu.csv')
+
+        new_data = pd.DataFrame({
             'Il': [Il],
             'Sicaklikk': [Sicaklik],
             'havadurumu': [havadurumu]
         })
 
-        data = pd.read_csv('havadurumu.csv')
         data = data.append(req_data, ignore_index=True)
         data.to_csv('havadurumu.csv', index=False)
         return {'message': 'Record successfully added.'}, 200
 
 
-class Name(Resource):
-    def get(self, name):
+class Il(Resource):
+    def get(self, Il):
         data = pd.read_csv('havadurumu.csv')
         data = data.to_dict('records')
         for entry in data:
-            if entry['Il'] == name:
+            if entry['Il'] == Il:
                 return {'data': entry}, 200
         return {'message': 'Ilin hava durumu bilgisi bulunamadı!'}, 404
 
 # Add URL endpoints
 api.add_resource(Users, '/users')
-api.add_resource(Name, '/<string:name>')
+api.add_resource(Name, '/<string:Il>')
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0",port=6767)
